0bfe8a08eef0666e02528dfcfc4d277f4a644cb3
jessepunkspin <jessepunkspin@gmail.com>
2025-11-28 07:48:25 +0100

    Update web3modal.js

--- web3modal.js
+++ web3modal.js
@@ -1,39 +1,18 @@
-// web3modal.js
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
 
-import { defaultWagmiConfig } from "@web3modal/wagmi";
-import { createWeb3Modal } from "@web3modal/wagmi/react";
-
-import { base } from "viem/chains";
-import { http } from "wagmi";
-
-// ===== YOUR NFT CONTRACT =====
-export const CONTRACT_ADDRESS =
-  "0x6a0DbB7e447B8f681afbe5ec8b1573e6FaFf7ba2";
-
-// ===== YOUR REOWN PROJECT ID =====
 export const projectId = "383e475c0afdbbbff941ef2b8febfabf";
 
-// ===== METADATA =====
-const metadata = {
-  name: "JessePunk Mint",
-  description: "Mint your JessePunk NFT",
-  url: "https://d225.vercel.app",
-  icons: ["https://avatars.githubusercontent.com/u/37784886"]
-};
-
-// ===== WAGMI CONFIG =====
-export const config = defaultWagmiConfig({
-  chains: [base],
+export const config = defaultConfig({
   projectId,
-  metadata,
-  transports: {
-    [base.id]: http()
+  metadata: {
+    name: 'JessePunk Mint',
+    description: 'Mint JessePunk NFT',
+    url: 'https://jessepunk.vercel.app',
+    icons: ['https://i.imgur.com/5cX9Z4P.png']
   }
 });
 
-// ===== INIT WEB3MODAL =====
 createWeb3Modal({
-  wagmiConfig: config,
-  projectId,
-  chains: [base]
+  ethersConfig: config,
+  projectId
 });
