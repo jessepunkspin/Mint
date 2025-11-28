c556d3c4de23302c92fe629ab1ec96091dbebd43
jessepunkspin <jessepunkspin@gmail.com>
2025-11-28 07:02:27 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
-import "../styles/globals.css";
-import "../web3modal"; // initialize modal globally
+import '../styles/globals.css'
+import { Web3Modal } from '@web3modal/wagmi/react'
+import { WagmiProvider } from 'wagmi'
+import { config, projectId } from '../web3modal'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </WagmiProvider>
+
+      <Web3Modal projectId={projectId} enableAnalytics={false} />
+    </>
+  )
 }
