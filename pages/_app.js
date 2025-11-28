852eff82c2fbc21ce969e02682f4c4f22ff2c307
jessepunkspin <jessepunkspin@gmail.com>
2025-11-28 08:00:01 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,5 @@
 import '../styles/globals.css'
-import { Web3Modal } from '@web3modal/ethers/react'
-import { projectId } from '../web3modal'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Web3Modal projectId={projectId} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
